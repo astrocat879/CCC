f15a7a4e82905564261c6c56235e6f2ab7b4bf80
N3rdSlqyer84 <45805218+N3rdSlqyer84@users.noreply.github.com>
2019-01-26 11:09:06 -0500

    Update J1.py

--- 2002/J1.py
+++ 2002/J1.py
@@ -1,14 +1,107 @@
-import sys                                      
-height = int(sys.stdin.readline())              
-counter = 1                                     
-shrinking = False                               
-for i in range(height):                         
-    a = '*' * counter                           
-    space = " " * ((height * 2) - (2 * len(a))) 
-    print(a + space + a)                        
-    if space == '':                             
-        shrinking = True                        
-    if shrinking:                               
-        counter -= 2                            
-    else:                                       
-        counter += 2                            
+zero = """ * * *
+*     *
+*     *
+*     *
+
+*     *
+*     *
+*     *
+ * * *"""
+
+one = """      *
+      *
+      *
+
+      *
+      *
+      *"""
+two = """ * * *
+      *
+      *
+      *
+ * * *
+*
+*
+*
+ * * *"""
+three = """ * * * 
+      *
+      *
+      *
+ * * * 
+      *
+      *
+      *
+ * * *"""
+four = """*     *
+*     *
+*     *
+ * * *
+      *
+      *
+      *"""
+five = """ * * *
+*
+*
+*
+ * * *
+      *
+      *
+      *
+ * * *"""
+six = """ * * *
+*     
+*     
+*     
+ * * *
+*     * 
+*     *
+*     *
+ * * *"""
+seven = """ * * * 
+      *
+      *
+      *
+      
+      *
+      *
+      *"""
+eight = """ * * *
+*     *
+*     *
+*     *
+ * * *
+*     *
+*     *
+*     *
+ * * *"""
+nine = """ * * *
+*     *
+*     *
+*     *
+ * * *
+      *
+      *
+      *
+ * * *"""
+a = input()
+if a == '0':
+    print(zero)
+elif a == '1':
+    print(one)
+elif a == '2':
+    print(two)
+elif a == '3':
+    print(three)
+elif a == '4':
+    print(four)
+elif a == '5':
+    print(five)
+elif a == '6':
+    print(six)
+elif a == '7':
+    print(seven)
+elif a == '8':
+    print(eight)
+elif a == '9':
+    print(nine)
